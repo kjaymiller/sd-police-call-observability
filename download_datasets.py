cbf30f682dc07a862ede44ebf894802780770d14
Jay Miller <kjaymiller@jays-mbp-2.lan>
2020-10-12 18:12:36 -0700

    reformat with black

--- download_datasets.py
+++ download_datasets.py
@@ -4,6 +4,7 @@ import httpx
 import json
 import asyncio
 
+
 async def download_file(filename):
     async with httpx.AsyncClient() as client:
         response = await client.get(filename)
@@ -11,24 +12,27 @@ async def download_file(filename):
 
 
 async def write_file(filename, content):
-    Path(filename.split('/')[-1]).write_bytes(content)
+    Path(filename.split("/")[-1]).write_bytes(content)
+
 
 async def get_files(filename):
-    content = await(download_file(filename))
+    content = await (download_file(filename))
     await write_file(filename, content)
 
-async def download(files: List[str]=None, include_resources: bool=False):
-    """downloads the keys to download new datafiles. If None, download all datasets. 
+
+async def download(files: List[str] = None, include_resources: bool = False):
+    """downloads the keys to download new datafiles. If None, download all datasets.
     Optionally download resources
     """
-    with open ('datasets.json') as fp:
+    with open("datasets.json") as fp:
         json_data = json.load(fp)
-    
+
     queue = []
 
-    for url in [x for x in json_data['datasets'].values()]:
+    for url in [x for x in json_data["datasets"].values()]:
         queue.append(get_files(url))
     await asyncio.wait(queue)
+
 
 if __name__ == "__main__":
     asyncio.run(download())
