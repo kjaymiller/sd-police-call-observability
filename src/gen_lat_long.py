3b083a3d39384410d9cdca7caf524bcc13d00257
Jay Miller <kjaymiller@jays-mbp-2.lan>
2020-10-15 14:34:39 -0700

    update gen lat/lon

--- src/gen_lat_long.py
+++ src/gen_lat_long.py
@@ -1,3 +1,4 @@
+import click
 from datetime import time
 from pathlib import Path
 from elasticsearch import Elasticsearch
@@ -11,13 +12,18 @@ es = Elasticsearch()
 geodf = eland.DataFrame(es, "sd_geo")
 
 
-def get_location_points(gdf, point, threshold=100):
+def get_closest_lat_long(point, threshold=100):
     q = {
+        "size": 1,
         "query": {
             "bool": {
                 "must": [
-                    {"match_all": {}},
-                ],
+                    {
+                        "wildcard": {
+                            "street.keyword": f"{point.address_road_primary}*",
+                            },
+                        },
+                    ],
                 "filter": {
                     "range": {
                         "number": {
@@ -28,29 +34,19 @@ def get_location_points(gdf, point, threshold=100):
                 },
             },
         },
-    }
-
-    results = gdf.es_query(q)
-    lat = results["latitude"].median()
-    lon = results["longitude"].median()
-
-    return [lat, lon]
-
-
-def get_location(df, point: str) -> typing.Tuple[int]:
-    """Check the lat/long of a document and add return the median object"""
-
-    q = {
-        "query": {
-            "wildcard": {
-                "street.keyword": f"{point}*",
-            },
+        "sort": {
+            "_script": {
+                "type": "number",
+                "script": {
+                    "lang": "painless",
+                    "source": f"Math.abs(doc['number'].value-{point.address_number_primary})",
+                },
+                "order": "asc",
+            }
         },
     }
-    gdf = geodf.es_query(q)
-    new_df = df.copy()
-    new_df["location"] = df.apply(lambda x: get_location_points(gdf, x), axis=1)
-    return new_df
+    result = geodf.es_query(q)
+    return [result.latitude, result.longitude]
 
 
 def build_datetimes(df: pd.DataFrame) -> pd.DataFrame:
@@ -60,7 +56,10 @@ def build_datetimes(df: pd.DataFrame) -> pd.DataFrame:
 
 
 @click.command()
-@click.argument("--filepath", help="CSV File to be processed and indexed")
+@click.argument(
+    "filepath",
+    type=click.Path(exists=True),
+)
 def build_index(filepath):
     """Load the corresponding csv file into a pandas dataframe and query each
     point against our sd_geo index appending the median lat/longs returned
@@ -70,24 +69,19 @@ def build_index(filepath):
 
     # filters out only points with a valid address
     address_points = df.loc[(df.address_number_primary != 0)]
-    points = address_points.address_road_primary.unique()
-
-    for point in points:
-        click.echo(f"fecthing {point}")
-        road = df.loc[(df.address_road_primary == point)]
-        new_df = get_location(road, point)
-        eland.pandas_to_eland(
-            pd_df=new_df,
-            es_client=es,
-            es_dropna=True,
-            es_dest_index="pd_calls_for_service_2015",
-            es_if_exists="append",  # so that these items are overwriting existing records. This allows us to update existing indexes.
-            es_type_overrides={"date_time": "date"},
+    address_points["location"] = address_points.apply(lambda x: get_closest_lat_long(x), axis=1)
+    eland.pandas_to_eland(
+        pd_df=address_points,
+        es_client=es,
+        es_dropna=True,
+        es_dest_index=Path(filepath).stem,
+        es_if_exists="replace",  # so that these items are overwriting existing records. This allows us to update existing indexes.
+        es_type_overrides={"date_time": "date"},
         )
 
 
 if __name__ == "__main__":
-    gen_csv("./assets/pd_calls_for_service_2015_datasd.csv")
+    build_index()
 
     # for _,row in df.iterrows():
     #    print(get_location(row.address_road_primary, row.address_number_primary))
